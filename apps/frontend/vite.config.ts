be9d42223d9bc3cae1808344174d1a4b077f8c4e
kimyou1102 <yoosk5485@naver.com>
2026-01-14 20:06:20 +0900

    chore: rtl vitest msw 세팅

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -19,4 +19,9 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	test: {
+		environment: "jsdom",
+		setupFiles: "./src/setupTests.ts",
+		globals: true,
+	},
 });
