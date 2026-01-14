8d6b3eef947e8b76d15ac68053f5608759be472d
kimyou1102 <yoosk5485@naver.com>
2026-01-14 18:58:25 +0900

    feat: 고객목록 아이템 클릭시 모달 열리는 기능 구현

--- apps/frontend/src/pages/dashboard/components/ConsumerListSection.tsx
+++ apps/frontend/src/pages/dashboard/components/ConsumerListSection.tsx
@@ -4,10 +4,13 @@ import { ChangeEvent, useState } from 'react'
 import CustomerPurchaseTable from './CustomerPurchaseTable'
 import { useCustomersQuery } from '@/pages/hooks/useCustomersQuery'
 import Pagination from './Pagination'
+import Modal from '@/components/Modal'
+import PurchaseItemTable from './PurchaseItemsTable'
 
 function ConsumerListSection() {
   const [dateRange, setDateRange] = useState({ start: '', end: '' })
   const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('desc')
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleChangeStartDate = (date: string) => {
     setDateRange((prev) => ({ ...prev, start: date }))
@@ -58,6 +61,14 @@ function ConsumerListSection() {
     }
   }
 
+  const handleCloseClick = () => {
+    setIsOpen(false)
+  }
+
+  const handleRowClick = (id: number) => {
+    setIsOpen(true)
+  }
+
   return (
     <S_Container>
       <ConsumerListToolbar
@@ -72,8 +83,22 @@ function ConsumerListSection() {
         rows={rows}
         sortDirection={sortDirection}
         onClickSortTotalAmount={handleClickSortTotalAmount}
+        onItemClick={handleRowClick}
       />
       <Pagination page={currentPage} totalPages={totalPages} onPrev={handlePrevClick} onNext={handleNextClick} />
+      <Modal isOpen={isOpen} title="" onClose={handleCloseClick}>
+        <PurchaseItemTable
+          rows={[
+            {
+              date: '2025-10-07',
+              quantity: 1,
+              product: '네이비 맨투맨',
+              price: 31000, // 제품 단가
+              imgSrc: 'http://localhost:4000/imgs/sweatshirt.jpg',
+            },
+          ]}
+        />
+      </Modal>
     </S_Container>
   )
 }
