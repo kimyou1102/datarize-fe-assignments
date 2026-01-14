014b5d700caa8ec36de775511e857203248ef3c9
kimyou1102 <yoosk5485@naver.com>
2026-01-14 19:15:07 +0900

    feat: 대시포드 페이지 구현

--- apps/frontend/src/pages/dashboard/Dashboard.tsx
+++ apps/frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,27 @@
+import styled from '@emotion/styled'
+import Header from './components/Header'
+import PriceRangeFrequencySection from './components/PriceRangeFrequencySection'
+import ConsumerListSection from './components/ConsumerListSection'
+
 function Dashboard() {
-  return <div>대시보드</div>
+  return (
+    <S_Container>
+      <Header />
+      <S_Main>
+        <PriceRangeFrequencySection />
+        <ConsumerListSection />
+      </S_Main>
+    </S_Container>
+  )
 }
 
 export default Dashboard
+
+const S_Container = styled.div``
+
+const S_Main = styled.main`
+  display: flex;
+  width: 100%;
+  padding: 2rem;
+  gap: 3rem;
+`
